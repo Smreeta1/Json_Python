c7bd2a3c6b968d1a20bf2dbb034a0c8c8dcb727b
smreeta <smreetashrestha1@gmail.com>
2024-07-08 23:35:29 +0545

    data removed from add_record.py

--- Operations/add_record.py
+++ Operations/add_record.py
@@ -10,5 +10,5 @@ def add_person(file_path, new_person):
     with open(file_path, 'w') as file:     #Write to file
         json.dump(data, file,indent=2)
 
-new_person = {"name": "new2", "age": 24, "city": "BRT"}  #new person record
+
 file_path = "./data.json" 
