05834e9d7ffe392d5efac412a1f4b5fe75573dc8
smreeta <smreetashrestha1@gmail.com>
2024-07-08 23:30:54 +0545

    function call removed from add_record.py

--- Operations/add_record.py
+++ Operations/add_record.py
@@ -1,10 +1,5 @@
 import json
-
-#Steps:Reading, Appending, and Writing
- #Direct dump() will overwrite existing dtaa
-        #new_person = {"name": "Hari", "age": 24, "city": "BRT"}
-        #with open(file_path, 'w') as file:
-        #json.dump([new_person], file, indent=2)  
+ 
          
 def add_person(file_path, new_person):
     with open(file_path, 'r') as file:      #Read file
@@ -15,8 +10,5 @@ def add_person(file_path, new_person):
     with open(file_path, 'w') as file:     #Write to file
         json.dump(data, file,indent=2)
 
-new_person = {"name": "Hari", "age": 24, "city": "BRT"}  #new person record
-
+new_person = {"name": "new2", "age": 24, "city": "BRT"}  #new person record
 file_path = "./data.json" 
-
-add_person(file_path, new_person) # Adding the new person record
