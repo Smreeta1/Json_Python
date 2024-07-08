98bb6dbefbe7edfdd22700fc6a4b7d274201e9dc
smreeta <smreetashrestha1@gmail.com>
2024-07-08 17:59:31 +0545

    name corrected

--- dump.py
+++ dump.py
@@ -1,17 +1,14 @@
 import json
 
-# Define a list of dictionaries
+#list of dictionaries definition
 data = [
     {"name": "Ram", "age": 20, "city": "ktm"},
     {"name": "Sita", "age": 22, "city": "Ktm"},
     {"name": "Abc", "age": 32, "city": "Bkt"}
 ]
-
-# Specify the file path where you want to save the JSON data
 file_path = "./data.json"
 
-# Open the file in write mode and use json.dump() to serialize data and write to file
-with open(file_path, "w") as json_file:
+with open(file_path, "w") as json_file:  #to serialize data and write to file
     json.dump(data, json_file)
 
 print(f"Data has been written to {file_path}")
